dd4f17dbbbae78b640861e4ab6b0368a31a431e0
gchqdev227 <62302861+gchqdev227@users.noreply.github.com>
2020-03-25 13:54:15 +0000

    Added tests for randomness

--- tests/test_random.py
+++ tests/test_random.py
@@ -4,6 +4,8 @@ Tests for reproducible randomness.
 import random
 import unittest
 
+from .utils import old_randint
+
 
 class RandomTests(unittest.TestCase):
 
@@ -17,6 +19,13 @@ class RandomTests(unittest.TestCase):
     def test_random_integers(self):
         """Test that the same random integers are generated."""
         random.seed(1)
-        observed_ints = [random.randint(0, 1000) for __ in range(9)]
+        observed_ints = [old_randint(0, 1000) for __ in range(9)]
         expected_ints = [134, 848, 764, 255, 495, 449, 652, 789, 93]
         self.assertListEqual(observed_ints, expected_ints)
+
+    def test_random_uniform(self):
+        """Test that the same uniformly distributed values are generated."""
+        random.seed(1)
+        observed_values = [random.uniform(0, 1000) for __ in range(4)]
+        expected_values = [134.36424411240122, 847.4337369372327, 763.7746189766141, 255.0690257394217]
+        self.assertListEqual(observed_values, expected_values)

f374c6c576d2651783d446f6abb58045efcb92ef
gchqdev227 <62302861+gchqdev227@users.noreply.github.com>
2020-03-25 16:49:35 +0000

    Refactored methods to separate file io from corpus creation

--- pdmm/corpus.py
+++ pdmm/corpus.py
@@ -40,26 +40,32 @@ class Corpus:
     @classmethod
     def from_document_file(cls, file_path):
         """Create a Corpus instance from a document file."""
+        with open(file_path, "r") as rf:
+            list_of_documents = [line.rstrip().split() for line in rf.readlines()]
+
+        return cls.from_documents_as_lists_of_words(list_of_documents)
+
+    @classmethod
+    def from_documents_as_lists_of_words(cls, list_of_documents):
+        """Create a Corpus instance from a list of lists of words."""
         vocab = Vocabulary()
         documents = []
         occurrence_to_index_count = []
 
-        with open(file_path, "r") as rf:
-            for line in rf.readlines():
-                document = []
-                word_occurrence_to_index_in_document_count = Counter()
-                word_occurrence_to_index_in_document = []
+        for list_of_words in list_of_documents:
+            document = []
+            word_occurrence_to_index_in_document_count = Counter()
+            word_occurrence_to_index_in_document = []
 
-                words = line.rstrip().split()
-                for word in words:
-                    word_id = vocab.get_id_from_word(word)
-                    document.append(word_id)
-                    word_occurrence_to_index_in_document_count += 1
+            for word in list_of_words:
+                word_id = vocab.get_id_from_word(word)
+                document.append(word_id)
+                word_occurrence_to_index_in_document_count += 1
 
-                    word_occurrence_to_index_in_document.append(word_occurrence_to_index_in_document_count[word])
+                word_occurrence_to_index_in_document.append(word_occurrence_to_index_in_document_count[word])
 
-                documents.append(document)
-                occurrence_to_index_count.append(word_occurrence_to_index_in_document)
+            documents.append(document)
+            occurrence_to_index_count.append(word_occurrence_to_index_in_document)
 
         word_counts_in_documents = np.array([np.bincount(document, minlength=vocab.size) for document in documents])
         return cls(documents, occurrence_to_index_count, word_counts_in_documents, vocab)

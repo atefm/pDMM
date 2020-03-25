3860a93ac081f111360096b39652eb96e3a51be3
gchqdev227 <62302861+gchqdev227@users.noreply.github.com>
2020-03-25 10:29:07 +0000

    Fixed misspelling of parameters

--- pdmm/pDMM.py
+++ pdmm/pDMM.py
@@ -14,7 +14,7 @@ def is_valid_file(parser, data_file):
 
 def check_arg(args=None):
     parser = argparse.ArgumentParser(description='Topic Modelling')
-    parser.add_argument('-i', '--corpus', help='Path to corpus file', required='True', metavar="<path>",
+    parser.add_argument('-i', '--corpus', help='Path to corpus file', required=True, metavar="<path>",
                         type=lambda corpus_file: is_valid_file(parser, corpus_file))
     parser.add_argument('-o', '--output', help='Output directory', default="./output/", metavar="<path>")
     parser.add_argument('-nt', '--ntopics', help='Number of topics', default=20, metavar="<integer>")
@@ -23,8 +23,8 @@ def check_arg(args=None):
     parser.add_argument('-ni', '--niters', help='Number of iterations', default=2000, metavar="<intege>")
     parser.add_argument('-t', '--twords', help='Number of most probable topical words', default=20, metavar="<integer>")
     parser.add_argument('-e', '--name', help='Name of the experiment', default="model", metavar="<string>")
-    paramters = parser.parse_args(args)
-    return paramters
+    parameters = parser.parse_args(args)
+    return parameters
 
 
 if __name__ == '__main__':

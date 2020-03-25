88af7950897b1190be40f693704027abdc7a65b7
gchqdev227 <62302861+gchqdev227@users.noreply.github.com>
2020-03-25 13:41:55 +0000

    Added test for sequences of random integers

--- tests/test_random.py
+++ tests/test_random.py
@@ -13,3 +13,10 @@ class RandomTests(unittest.TestCase):
         observed_floats = [random.random() for __ in range(4)]
         expected_floats = [0.13436424411240122, 0.8474337369372327, 0.763774618976614, 0.2550690257394217]
         self.assertListEqual(observed_floats, expected_floats)
+
+    def test_random_integers(self):
+        """Test that the same random integers are generated."""
+        random.seed(1)
+        observed_ints = [random.randint(0, 1000) for __ in range(9)]
+        expected_ints = [134, 848, 764, 255, 495, 449, 652, 789, 93]
+        self.assertListEqual(observed_ints, expected_ints)

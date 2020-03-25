142baac63fba81726b979ad079039d7647fd71c9
gchqdev227 <62302861+gchqdev227@users.noreply.github.com>
2020-03-25 16:09:23 +0000

    Added imports, module docstring and version number

--- pdmm/__init__.py
+++ pdmm/__init__.py
@@ -1,1 +1,8 @@
+"""
+Python3 implementation for Dirichlet multinomial mixture model.
+"""
+from .corpus import Corpus
+from .sampling import GibbsSamplingDMM
 from .vocabulary import Vocabulary
+
+__version__ = "1.0.0"

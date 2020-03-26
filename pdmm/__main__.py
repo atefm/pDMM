0e8557c6fde350437838c75954502af8f54937a0
gchqdev227 <62302861+gchqdev227@users.noreply.github.com>
2020-03-26 15:11:07 +0000

    Created main function for scoping purposes

--- pdmm/__main__.py
+++ pdmm/__main__.py
@@ -1,9 +1,37 @@
+"""
+Contains the main function for running inference from the command line.
+"""
 import argparse
+import logging
 import os
 import sys
 
 from .corpus import Corpus
 from .sampling import GibbsSamplingDMM
+
+
+def main(parameters):
+    """Main function."""
+    logger = logging.getLogger(__name__)
+
+    corpus = Corpus.from_document_file(parameters.corpus_path)
+
+    model = GibbsSamplingDMM(
+        corpus,
+        parameters.output_path,
+        parameters.number_of_topics,
+        parameters.alpha,
+        parameters.beta,
+        parameters.number_of_iterations,
+        parameters.number_of_top_words,
+        parameters.name
+    )
+    model.topic_assignment_initialise()
+    model.inference()
+
+    logger.debug("Writing Results")
+    model.save_top_topical_words_to_file(parameters.output_path + parameters.name + ".topWords")
+    model.save_topic_assignments_to_file(parameters.output_path + parameters.name + ".topicAssignments")
 
 
 def check_arg(args=None):
@@ -21,21 +49,5 @@ def check_arg(args=None):
 
 
 if __name__ == '__main__':
-    args = check_arg(sys.argv[1:])
-    corpus = Corpus.from_document_file(args.output_path)
-    model = GibbsSamplingDMM(
-        corpus,
-        args.output_path,
-        args.number_of_topics,
-        args.alpha,
-        args.beta,
-        args.number_of_iterations,
-        args.number_of_top_words,
-        args.name
-    )
-    model.topic_assignment_initialise()
-    model.inference()
-
-    print("Writing Results")
-    model.save_top_topical_words_to_file(args.output + args.name + ".topWords")
-    model.save_topic_assignments_to_file(args.output + args.name + ".topicAssignments")
+    parsed_parameters = check_arg(sys.argv[1:])
+    main(parsed_parameters)

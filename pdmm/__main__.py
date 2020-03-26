a41dae47c48a2f2a6f380874427ac1fab49d184e
gchqdev227 <62302861+gchqdev227@users.noreply.github.com>
2020-03-26 15:27:07 +0000

    Updated args passed to GibbsSamplingDMM constuctor

--- pdmm/__main__.py
+++ pdmm/__main__.py
@@ -18,13 +18,10 @@ def main(parameters):
 
     model = GibbsSamplingDMM(
         corpus,
-        parameters.output_path,
         parameters.number_of_topics,
         parameters.alpha,
         parameters.beta,
-        parameters.number_of_iterations,
-        parameters.number_of_top_words,
-        parameters.name
+        parameters.number_of_iterations
     )
     model.topic_assignment_initialise()
     model.inference()
@@ -37,14 +34,17 @@ def main(parameters):
 
 def check_arg(args=None):
     parser = argparse.ArgumentParser(description='Topic Modelling')
-    parser.add_argument('-c', '--corpus-file', help='Path to corpus file', dest="corpus_path", required=True, metavar="<path>")
-    parser.add_argument('-o', '--output-path', help='Output directory', dest="output_path", default="./output/", metavar="<path>")
+    parser.add_argument('-c', '--corpus-file', help='Path to corpus file', dest="corpus_path", required=True,
+                        metavar="<path>")
     parser.add_argument('-n', '--num-topics', help='Number of topics', dest="number_of_topics", default=20, metavar="<integer>", type=int)
     parser.add_argument('-a', '--alpha', help='Alpha value', default=0.1, metavar="<double>", type=float)
     parser.add_argument('-b', '--beta', help='Beta value', default=0.001, metavar="<double>", type=float)
-    parser.add_argument('-ni', '--iterations', help='Number of iterations', dest="number_of_iterations", default=2000, metavar="<integer>", type=int)
-    parser.add_argument('-t', '--num_words', help='Number of most probable topical words', dest="number_of_top_words", default=20, metavar="<integer>", type=int)
-    parser.add_argument('-e', '--name', help='Name of the experiment', default="model", metavar="<string>")
+    parser.add_argument('--output-path', help='Output directory', dest="output_path", default="./output/",
+                        metavar="<path>")
+    parser.add_argument('--iterations', help='Number of iterations', dest="number_of_iterations", default=2000,
+                        metavar="<integer>", type=int)
+    parser.add_argument('--num_words', help='Number of most probable topical words', dest="number_of_top_words",
+                        default=20, metavar="<integer>", type=int)
     parameters = parser.parse_args(args)
     return parameters
 

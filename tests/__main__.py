255094b7acd50054435be7f7a35441bd140730c1
gchqdev227 <62302861+gchqdev227@users.noreply.github.com>
2020-03-26 12:23:29 +0000

    Added coverage

--- tests/__main__.py
+++ tests/__main__.py
@@ -3,12 +3,20 @@ Runs the tests.
 """
 import unittest
 
+import coverage
+
 
 def run_tests():
     test_loader = unittest.TestLoader()
     all_tests = test_loader.discover(".")
     test_runner = unittest.TextTestRunner()
+
+    cov = coverage.Coverage(source=["pdmm"])
+    cov.start()
     test_runner.run(all_tests)
+    cov.stop()
+
+    cov.html_report()
 
 
 if __name__ == "__main__":

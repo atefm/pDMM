793c14db2cf4f107eb2f421a9fb9fab750b34370
gchqdev227 <62302861+gchqdev227@users.noreply.github.com>
2020-03-26 14:50:52 +0000

    Adjusted command line arguments

--- pdmm/__main__.py
+++ pdmm/__main__.py
@@ -5,23 +5,15 @@ import sys
 from .sampling import GibbsSamplingDMM
 
 
-def is_valid_file(parser, data_file):
-    if not os.path.exists(data_file):
-        parser.error("The file \"%s\" does not exist!" % data_file)
-    else:
-        return data_file
-
-
 def check_arg(args=None):
     parser = argparse.ArgumentParser(description='Topic Modelling')
-    parser.add_argument('-i', '--corpus', help='Path to corpus file', required=True, metavar="<path>",
-                        type=lambda corpus_file: is_valid_file(parser, corpus_file))
-    parser.add_argument('-o', '--output', help='Output directory', default="./output/", metavar="<path>")
-    parser.add_argument('-nt', '--ntopics', help='Number of topics', default=20, metavar="<integer>")
+    parser.add_argument('-c', '--corpus-file', help='Path to corpus file', dest="corpus_path", required=True, metavar="<path>")
+    parser.add_argument('-o', '--output-path', help='Output directory', dest="output_path", default="./output/", metavar="<path>")
+    parser.add_argument('-n', '--num-topics', help='Number of topics', dest="number_of_topics", default=20, metavar="<integer>")
     parser.add_argument('-a', '--alpha', help='Alpha value', default=0.1, metavar="<double>")
     parser.add_argument('-b', '--beta', help='Beta value', default=0.001, metavar="<double>")
-    parser.add_argument('-ni', '--niters', help='Number of iterations', default=2000, metavar="<intege>")
-    parser.add_argument('-t', '--twords', help='Number of most probable topical words', default=20, metavar="<integer>")
+    parser.add_argument('-ni', '--iterations', help='Number of iterations', dest="number_of_iterations", default=2000, metavar="<integer>")
+    parser.add_argument('-t', '--num_words', help='Number of most probable topical words', dest="number_of_top_words", default=20, metavar="<integer>")
     parser.add_argument('-e', '--name', help='Name of the experiment', default="model", metavar="<string>")
     parameters = parser.parse_args(args)
     return parameters
@@ -30,13 +22,13 @@ def check_arg(args=None):
 if __name__ == '__main__':
     args = check_arg(sys.argv[1:])
     model = GibbsSamplingDMM(
-        args.corpus,
-        args.output,
-        args.ntopics,
+        args.corpus_path,
+        args.output_path,
+        args.number_of_topics,
         args.alpha,
         args.beta,
-        args.niters,
-        args.twords,
+        args.number_of_iterations,
+        args.number_of_top_words,
         args.name
     )
     model.analyse_corpus()

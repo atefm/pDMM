244a2d30dbd9306381586922dd5045d5d208952b
gchqdev227 <62302861+gchqdev227@users.noreply.github.com>
2020-03-27 15:26:17 +0000

    Removed redundant function to seed numpy random number generator

--- tests/utils.py
+++ tests/utils.py
@@ -11,12 +11,3 @@ def read_contents_from_path(file_path):
     with open(file_path, "r") as rf:
         contents = rf.read()
     return contents
-
-
-def seed_numpy_with_same_seed_as_random(seed=None):
-    """Seed the numpy random generator to the same state as the Python one."""
-    standard_random_generator = random.Random(seed)
-    version, (*mt_state, position), gauss_next = standard_random_generator.getstate()
-    new_numpy_state = ("MT19937", mt_state, position)
-    # noinspection PyTypeChecker
-    np.random.set_state(new_numpy_state)

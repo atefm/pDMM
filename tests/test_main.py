daf28b1c5bb577fc16365e157b8e0189bd0647b8
gchqdev227 <62302861+gchqdev227@users.noreply.github.com>
2020-03-27 12:13:54 +0000

    Replaced tempdir with tempfile.TemporaryDirectory to ensure proper cleanup

--- tests/test_main.py
+++ tests/test_main.py
@@ -15,14 +15,21 @@ from .utils import read_contents_from_path, python_2_randint
 
 class CommandLineTests(unittest.TestCase):
 
+    def setUp(self):
+        """Code to run before each test."""
+        self.tempdir = tempfile.TemporaryDirectory()
+
+    def tearDown(self):
+        """Code to run after each test."""
+        self.tempdir.cleanup()
+
     def test_overall_output(self):
         """Test that the output is correct."""
         random.seed(1)
         pdmm.sampling.random.randint = python_2_randint
-        temp_dir = tempfile.gettempdir() + "/"
         arg_string = "--corpus {} --output {} --iterations {}".format(
             "tests/data/sample_data",
-            temp_dir,
+            self.tempdir.name,
             50,
         )
         arguments_components = arg_string.split()
@@ -33,8 +40,8 @@ class CommandLineTests(unittest.TestCase):
         expected_top_words = read_contents_from_path("tests/data/topWords")
         expected_topic_assignments = read_contents_from_path("tests/data/topicAssignments")
 
-        top_words = read_contents_from_path(os.path.join(temp_dir, "model.topWords"))
-        topic_assignments = read_contents_from_path(os.path.join(temp_dir, "model.topicAssignments"))
+        top_words = read_contents_from_path(os.path.join(self.tempdir.name, "topWords"))
+        topic_assignments = read_contents_from_path(os.path.join(self.tempdir.name, "topicAssignments"))
 
         self.assertEqual(top_words, expected_top_words, "Top words should be equal.")
         self.assertEqual(topic_assignments, expected_topic_assignments, "Topic assignments should be equal.")

fd040c6910f80761633f747e91cd4add5d82e55e
gchqdev227 <62302861+gchqdev227@users.noreply.github.com>
2020-03-30 16:13:15 +0100

    Refactored to work with np.cumsum

--- pdmm/utils.py
+++ pdmm/utils.py
@@ -26,19 +26,17 @@ def sample_from_multinomial_and_mutate_weights(weights, random_number):
     - Sampling like this will mutate the input array.
     - Weights do NOT need to sum to 1.
     """
-    weights = weights.copy()
+    cumulative_weights = weights.cumsum()
     number_of_weights = weights.shape[0]
-    for i in range(1, number_of_weights):
-        weights[i] += weights[i - 1]
 
-    scaled_random_number = random_number * weights[-1]
+    scaled_random_number = random_number * cumulative_weights[-1]
 
     counter = 0
     maximum_counter_value = number_of_weights - 1
 
     while counter < maximum_counter_value:
         mid = counter + ((maximum_counter_value - counter) // 2)
-        if scaled_random_number > weights[mid]:
+        if scaled_random_number > cumulative_weights[mid]:
             counter = mid + 1
         else:
             maximum_counter_value = mid

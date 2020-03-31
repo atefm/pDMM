a6a825c319cda5f81a1facece78a0fa8a44ea14c
gchqdev227 <62302861+gchqdev227@users.noreply.github.com>
2020-03-31 12:57:37 +0100

    Added new test for multiple sampling in parallel

--- tests/test_random.py
+++ tests/test_random.py
@@ -5,7 +5,7 @@ import unittest
 
 import numpy as np
 
-from pdmm.utils import sample_from_cumulative_weights
+import pdmm.utils
 
 
 class RandomTests(unittest.TestCase):
@@ -13,18 +13,24 @@ class RandomTests(unittest.TestCase):
     def setUp(self):
         """Code to run before each test."""
         np.random.seed(19)
-        self.weights = np.random.random(20)
+        weights = np.random.random(20)
+        self.cumulative_weights = weights.cumsum()
 
         np.random.seed(5)
         self.random_numbers = np.random.random(20)
-        self.expected_values = [1, 6, 1, 6, 6, 6, 6, 6, 1, 1, 0, 6, 4, 1, 6, 1, 4, 1, 6, 6]
+        self.expected_values = [6, 17, 6, 18, 9, 11, 15, 9, 7, 5, 1, 14, 8, 4, 18, 7, 8, 7, 11, 11]
 
     def test_random_sampling(self):
         """Test that the cumulative sampling is reproducible.."""
         sampled_values = []
 
         for random_number in self.random_numbers:
-            sampled_value = sample_from_cumulative_weights(self.weights, random_number)
+            sampled_value = pdmm.utils.sample_from_cumulative_weights(self.cumulative_weights, random_number)
             sampled_values.append(sampled_value)
 
         self.assertListEqual(sampled_values, self.expected_values)
+
+    def test_multiple_sampling(self):
+        """Test that sampling all at works produces the same results."""
+        sampled_values = pdmm.utils.sample_many_from_cumulative_weights(self.cumulative_weights, self.random_numbers)
+        self.assertListEqual(list(sampled_values), self.expected_values)

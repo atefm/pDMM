40c46aa53950f9205113ce6ba2e3cc26af1dff1a
gchqdev227 <62302861+gchqdev227@users.noreply.github.com>
2020-04-09 16:10:56 +0100

    Cited paper in module docstring

--- pdmm/__init__.py
+++ pdmm/__init__.py
@@ -1,5 +1,9 @@
 """
 Python 3 implementation for Dirichlet multinomial mixture model.
+
+Implementation based on the paper "A Dirichlet Multinomial Mixture Model-based
+Approach for Short Text Clustering" by Jianhua Yin and Jianyong Wang, which can
+be found at https://dl.acm.org/doi/10.1145/2623330.2623715.
 """
 from .corpus import Corpus
 from .sampling import GibbsSamplingDMM
